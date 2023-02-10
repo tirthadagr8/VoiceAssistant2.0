f2d2d18a64f84de5732d056387032063682a33fe
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2023-02-10 22:57:12 +0530

    Update spid.py

--- speaker/spid.py
+++ speaker/spid.py
@@ -1,7 +1,7 @@
-username = 'ajqiq27gjd4sfwauyb10lj8by'
-client_id = '16b748ef50dd49a3967e215f62769fb0'
-client_secret = '63eb2c1ad0824c959973ef87f5e9a3e9'
+username = ''    ## Get the details from spotify profile
+client_id = ''
+client_secret = ''
 redirect_uri = 'http://google.com/callback/'
-device_id1='420ba48deddde6409f5350d6b7880ce6437b7a64' ## LAPTOP
-device_id2='bf2145053ac6e60f7ee0009f1bbe7c5062d4668f' ## SAMSUNG PHONE
+device_id1='' ## LAPTOP
+device_id2='' ## SAMSUNG PHONE
 scope='user-read-private user-read-playback-state user-modify-playback-state'
